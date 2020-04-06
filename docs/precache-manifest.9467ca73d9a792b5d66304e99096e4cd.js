db6624eb2cd48ad38e2b4056d8d8b29e72ede155
Mrigank <mrigank.2245010@gmail.com>
2020-04-06 21:35:26 +0530

    Added docs folder for hosting

---
+++ docs/precache-manifest.9467ca73d9a792b5d66304e99096e4cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2b9fa94834cda0ce3faa9cda116535d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0c8e55ba53f2df297ad",
+    "url": "/static/css/main.261928f8.chunk.css"
+  },
+  {
+    "revision": "1df43436bd53d5dd5a77",
+    "url": "/static/js/2.f2dcd070.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f2dcd070.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0c8e55ba53f2df297ad",
+    "url": "/static/js/main.770f3f79.chunk.js"
+  },
+  {
+    "revision": "da30a97f7a1ee5d7af6d",
+    "url": "/static/js/runtime-main.68fb6744.js"
+  }
+]);
